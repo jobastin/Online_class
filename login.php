95536b42f2565ae0dcc00504b12109ed522dff5c
madhaven <jitinjacobgigi@mca.ajce.in>
2020-06-17 21:17:51 +0530

    login completed

--- login.php
+++ login.php
@@ -3,14 +3,26 @@ require('fun.php');
 sessiondelete();
 if (!isset($_POST['submit'])){
 ?><!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Login | St. Aloysius School</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
-	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<script>
+<meta charset="utf-8"/>
+<link rel="apple-touch-icon" sizes="76x76" href="./assets/img/favicon.ico">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+<title>Login | St. Aloysius School</title>
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport'/>
+    
+<!-- Google Font -->
+<link href="https://fonts.googleapis.com/css?family=Nunito:300,300i,400,600,800" rel="stylesheet">
+    
+<!-- Font Awesome Icons -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    
+<!-- Main CSS -->
+<link href="css/main.css" rel="stylesheet"/>
+    
+<!-- Animation CSS -->
+<link href="./assets/css/vendor/aos.css" rel="stylesheet"/>
+    <script>
         function checkuser(username){
             var ajax = new XMLHttpRequest();
             ajax.onreadystatechange = function(){
@@ -27,43 +39,86 @@ if (!isset($_POST['submit'])){
         }
     </script>
 </head>
-<body>
-	<img class="wave" src="img/background.jpg">
-	<div class="navbar" style="background-color: #25CCF7;opacity: 0.2">
-	   <center><b><h2>St. ALOYSIUS SCHOOL TANIKALLA</h2></b></center>
+    
+<body> 
+<nav class="topnav navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+<div class="container-fluid">
+	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="navbar-collapse collapse" id="navbarColor02" style="">
+		<ul class="navbar-nav mr-auto d-flex align-items-center">
+			<li class="nav-item">
+			<a class="nav-link" href="#">Home</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="#">Contact</a>
+			</li>
+		</ul>
 	</div>
-	<div class="banner-area"></div>
-	<div class="container">
-		<div class="img">
-			<img src="img/thumpnail.png">
+</div>
+</nav>
+<!-- End Navbar -->
+    
+<!-- Main -->
+<div class="d-md-flex h-md-100 align-items-center">
+	<div class="col-md-6 p-0 bg-indigo h-md-100" style="background-image: url(img/thumpnail.png); background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position:102px 54px;">
+		<div class="text-white d-md-flex align-items-center h-100 p-5 text-center justify-content-center">
 		</div>
-		<div class="login-content">
-			<form action="login.php" method="post">
-				<img src="img/avathar.png" style="border-radius: 50%;">
-				<h2 class="title">LOGIN</h2>
-           		<div class="input-div one">
-           		   <div class="i">
-           		   		<i class="fas fa-user"></i>
-           		   </div>
-           		   <div class="div">
-           		   		<h5>Student ID</h5>
-           		   		<input type="text" class="input" name="username" oninput='checkuser(this.value);'>
-           		   </div>
-           		</div>
-           		<div class="input-div pass">
-           		   <div class="i"> 
-           		    	<i class="fas fa-lock"></i>
-           		   </div>
-           		   <div class="div">
-           		    	<h5>Password</h5>
-           		    	<input type="password" class="input" name="password">
-            	   </div>
-            	</div>
-            	<input type="submit" class="btn" value="Login" name='submit'>
-            </form>
-        </div>
-    </div>
-    <script type="text/javascript" src="js/main.js"></script>
+	</div>
+	<div class="col-md-6 p-0 bg-white h-md-100 loginarea" style="background-image: url(img/img11.JPG); background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 700px auto;
+  background-position:right top;
+  opacity: 0.6;">
+		<div class="d-md-flex align-items-center h-md-100 p-5 justify-content-center">
+			<form class="border rounded p-5" style="background-color: white;opacity: 1;" action="login.php" method="post">
+				<h3 class="mb-4 text-center">Sign In</h3>
+				<div class="form-group">
+					<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Login ID" required="" oninput="checkuser(this.value);" name="username">
+				</div>
+				<div class="form-group">
+					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required="" name="password">
+				</div>
+				<button type="submit" class="btn btn-success btn-round btn-block shadow-sm" name="submit">Sign in</button>
+			</form>
+		</div>
+	</div>
+</div>    
+<script src="./assets/js/vendor/jquery.min.js" type="text/javascript"></script>
+<script src="./assets/js/vendor/popper.min.js" type="text/javascript"></script>
+<script src="./assets/js/vendor/bootstrap.min.js" type="text/javascript"></script>
+<script src="./assets/js/functions.js" type="text/javascript"></script>
+    
+<!-- Animation -->
+<script src="./assets/js/vendor/aos.js" type="text/javascript"></script>
+<noscript>
+    <style>
+        *[data-aos] {
+            display: block !important;
+            opacity: 1 !important;
+            visibility: visible !important;
+        }
+    </style>
+</noscript>
+<script>
+    AOS.init({
+        duration: 700
+    });
+</script>
+ 
+<!-- Disable animation on less than 1200px, change value if you like -->
+<script>
+AOS.init({
+  disable: function () {
+    var maxWidth = 1200;
+    return window.innerWidth < maxWidth;
+  }
+});
+</script>
+
 </body>
 </html>
 <?php 
