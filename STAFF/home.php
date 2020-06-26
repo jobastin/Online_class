43be55457c19a9fd58a84dd85ad0acea542382a7
ADHIN BABU <adhinbabu1998@gmail.com>
2020-06-26 12:10:11 +0530

    ui update

--- STAFF/home.php
+++ STAFF/home.php
@@ -367,14 +367,28 @@ else{
           <!-- Topbar Navbar -->
           <h3>St. ALOYSIUS ENGLISH MEDIUM SCHOOL THANIKALLA</h3>
           <ul class="navbar-nav ml-auto">
+
+           
             <div class="topbar-divider d-none d-sm-block"></div>
+            
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
-                              <a class="dropdown-item" href="../logout.php" data-toggle="modal" data-target="#logoutModal" id="logout">
+              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Principal</span>
+                <img class="img-profile rounded-circle " src="../img/thumpnail.png"> 
+              </a>
+              <!-- Dropdown - User Information -->
+              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#myModal1">
+                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Profile
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="../logout.php" data-toggle="modal" data-target="#logoutModal" id="logout">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                   Logout
                 </a>
-              <!-- Dropdown - User Information -->
+              </div>
             </li>
           </ul>
         </nav>
@@ -561,6 +575,10 @@ else{
                 <input class="form-control" type="text" placeholder="Set Password">
             </div>
             <br>
+            <div class="custom-control custom-checkbox">
+          <input name="check3" value="admin" type="checkbox" class="custom-control-input" id="admincheck">
+          <label class="custom-control-label" for="admincheck">Admin User</label>
+        </div>
       </div>
 
       <!-- Modal footer -->
